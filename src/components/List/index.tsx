2a6b1dc8109f270803c0489e76c1a18f7c326f3b
MurlocoLoco <erickmartins0808@gmail.com>
2022-10-19 03:22:05 -0300

    Excluir Tarefa

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -5,24 +5,32 @@ const List = () => {
     const { tarefas, setTarefas } = useBase() as IContextTarefas;
 
     const selectTarefa = (id: string) => {
-        const selectedTarefa = tarefas.find(obj => {return obj.id === id});
-        if(selectedTarefa) {
+        const selectedTarefa = tarefas.find(obj => { return obj.id === id });
+        if (selectedTarefa) {
             const updatedTarefa: ITarefas = {
                 ...selectedTarefa,
-                completado: !selectedTarefa.completado 
+                completado: !selectedTarefa.completado
             }
             const result: ITarefas[] = tarefas.map(tarefa => tarefa.id === id ? updatedTarefa : tarefa);
             setTarefas(result);
         }
     }
+    const excluirTarefa = (id: string) => {
+        const selectedTarefa = tarefas.filter(obj => { return obj.id !== id });
+        setTarefas(selectedTarefa)
+    }
     return (
         <div className={styles.listMain}>
             <ul>
                 {tarefas.map(item => (
-                    <li onClick={() =>selectTarefa(item.id)} key={item.id} className={`${item.completado ?  styles.completado : styles.item}`}>
-                        <h2>{item.titulo}</h2>
-                        <h3>{item.descricao}</h3>
-                    </li>
+                    <div className={styles.divTeste}>
+                        <li onClick={() => selectTarefa(item.id)} key={item.id} className={`${item.completado ? styles.completado : styles.item}`}>
+                            <h2>{item.titulo}</h2>
+                            <h3>{item.descricao}</h3>
+                        </li>
+                        <div onClick={() => excluirTarefa(item.id)} className={styles.excluirItem}></div>
+                    </div>
+
                 ))}
             </ul>
         </div>
